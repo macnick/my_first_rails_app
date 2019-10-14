9efcc9122b95d2dee666b1e07a8773112442ede5
macnick <nick@macexperts.gr>
2019-10-14 12:31:03 +0300

    2nd initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 Rails.application.routes.draw do
-  root 'cars#index'
   resources :cars
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
